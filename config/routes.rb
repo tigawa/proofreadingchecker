e73c76ae0cdca5a0967d5d35b49bf83cd3f02e81
tigawa <ihciiat@gmail.com>
2013-09-28 22:10:08 +0900

    top indexを修正

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ RailsApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'sentence#index'
 
   # See how all your routes lay out with "rake routes"
 
